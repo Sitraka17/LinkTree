df56b0e78f06a33d89b50d7814c138e1d217f250
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-07-22 00:10:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,6 @@ st_button('youtube', 'https://www.youtube.com/channel/UC9SHkerBF56p-skyKbm7NpQ',
 st_button('youtube', 'https://youtube.com/codingprofessor', 'Sitraka Data YouTube channel', icon_size)
 st_button('medium', 'https://medium.com/@sitrakaforler', 'Read my Blogs', icon_size)
 st_button('twitter', 'https://twitter.com/ForlerSitraka', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/sitraka-matthieu-forler/', 'Follow me on LinkedIn', icon_size)
 st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
 st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
