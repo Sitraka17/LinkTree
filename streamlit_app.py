133de7da8882ec787e288496fb3e2f41b4ee24a0
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-07-22 00:12:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ st_button('medium', 'https://medium.com/@sitrakaforler', 'Read my Blogs', icon_s
 st_button('twitter', 'https://twitter.com/ForlerSitraka', 'Follow me on Twitter', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/sitraka-matthieu-forler/', 'Follow me on LinkedIn', icon_size)
 st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy a Coffee 2 the creator of this template!', icon_size)
